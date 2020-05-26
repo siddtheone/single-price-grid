f6c6002bf0359539351a8e2f0e88f6c28680a468
siddtheone <siddharthl@cybage.com>
2020-05-26 12:53:20 +0530

    Updates

---
+++ precache-manifest.e9d6dc3d10e631ecde2674da7d16a385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe2b96dc3c287576a416e3606db733e",
+    "url": "/single-price-grid/index.html"
+  },
+  {
+    "revision": "beb26d633ef15b4e8f5b",
+    "url": "/single-price-grid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "380760e2b1793c638603",
+    "url": "/single-price-grid/static/js/2.861fce4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/single-price-grid/static/js/2.861fce4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb26d633ef15b4e8f5b",
+    "url": "/single-price-grid/static/js/main.b42bb049.chunk.js"
+  },
+  {
+    "revision": "d653ca1eedcbdccf0fde",
+    "url": "/single-price-grid/static/js/runtime-main.b4238db7.js"
+  }
+]);
